eae1c5acdc22f142cbb85f29e367b0ce3e9e52bd
Jhonatan Vazquez <eric.jhonatanvazquez@gmail.com>
2026-01-20 22:28:39 -0600

    Refactoriza la estructura de archivos

--- src/skparate-viewer/skparate-viewer.js
+++ src/skparate-viewer/skparate-viewer.js
@@ -160,7 +160,7 @@ export class SkparateViewer extends LitElement {
               <div style="padding:40px">
                 <table>
                   <tr><th>Propiedad</th><th>Tipo</th><th>Descripción</th></tr>
-                  ${this.config?.props.map(p => html`<tr><td>${p.name}</td><td><code class="type-tag">${p.type}</code></td><td>${p.desc}</td></tr>`)}
+                  ${this.config?.props.map(p => html`<tr><td>${p.name}</td><td><code class="type-tag">${p.type}</code></td><td>${p.desc}</td></tr>`)} //FIXME Aquí se genera la tabla de propiedades de la documentación 
                 </table>
               </div>
             ` : ''}
